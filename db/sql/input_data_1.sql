923b798b257691df6b4d1375a53df7322b1ef966
Tony Forsythe <forsythetony@gmail.com>
2016-03-24 04:54:42 -0500

    tired

--- db/sql/input_data_1.sql
+++ db/sql/input_data_1.sql
@@ -3,4 +3,4 @@ USE linkedin_group_5;
 LOAD DATA LOCAL INFILE '../input_data/user_input_v1.csv' INTO TABLE user FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' (username, email);
 
 
-LOAD DATA LOCAL INFILE '../input_data/people_input_v1.csv' INTO TABLE person IGNORE 1 LINES FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' ('uid','firstname', 'middlename','lastname', 'birth_date');
+LOAD DATA LOCAL INFILE '../input_data/person_input_v1.csv' INTO TABLE person IGNORE 1 LINES FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' ('uid','firstname', 'middlename','lastname', 'birth_date');
