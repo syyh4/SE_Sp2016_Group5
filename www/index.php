ed3bddf51f7cc10767df24297324880b42c54227
syyh4 <syyh4@mail.missouri.edu>
2016-04-06 18:09:37 -0500

    evan2

--- www/index.php
+++ www/index.php
@@ -1,1 +1,7 @@
-<h1>Tesing</h1>
+<h1>
+This website is for Software Engineering Project from Group 5
+
+Make sure all codes are under SE_Sp2016_Group5/WWW
+
+After you push a commit, it will take at most 30 seconds to update.
+</h1>
