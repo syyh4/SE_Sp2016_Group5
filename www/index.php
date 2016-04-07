876ca0c507089fc1cbbd73e2f5e0627876e2f5d6
syyh4 <syyh4@mail.missouri.edu>
2016-04-06 19:04:35 -0500

    evan

--- www/index.php
+++ www/index.php
@@ -1,7 +1,9 @@
 <h1>
-This website is for Software Engineering Project from Group 5.\n
+This website is for Software Engineering Project from Group 5.
 
-Make sure all codes are under SE_Sp2016_Group5/WWW\n
+
+Make sure all codes are under SE_Sp2016_Group5/WWW
+
 
 After you push a commit, it will take at most 30 seconds to update.
 </h1>
