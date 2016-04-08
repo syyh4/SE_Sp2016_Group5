1c41dedac285f56c24eb7f448ae42d0007da4b1f
syyh4 <syyh4@mail.missouri.edu>
2016-04-08 11:17:27 -0500

    server_test

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Group  | LinkedIn</title>
+    <title>Group 5 | LinkedIn</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -110,7 +110,7 @@
       <div class="row">
         <div class="col-md-10 col-md-offset-1 box">
           <div class="col-md-12 text-center">
-            <h1 class="company-name">Group 5</h1>
+            <h1 class="company-name">Group </h1>
             <h2 class="lead location">Software Engineering</h2>
             <p class="company-desc"></p>
           </div>
