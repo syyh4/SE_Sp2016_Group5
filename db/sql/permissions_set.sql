2f6ae5da21dd3053ddb150f8870bb60ae3e00ff7
Tony Forsythe <forsythetony@gmail.com>
2016-04-10 18:36:11 -0500

    Fixes database reference in the permissions set SQL script

--- db/sql/permissions_set.sql
+++ db/sql/permissions_set.sql
@@ -1,3 +1,3 @@
 
 #	Make sure that the web user can access the 'linked_in_group_5' database
-GRANT CREATE, READ, UPDATE, DELETE ON linked_in_group_5.* TO 'web'@'localhost';
+GRANT CREATE, READ, UPDATE, DELETE ON linkedin_group_5.* TO 'web'@'localhost';
