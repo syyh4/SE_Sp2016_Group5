c1aa07e5f29f00f33a928f9ed55c869687964208
Tony Forsythe <forsythetony@gmail.com>
2016-04-14 18:27:46 -0500

    updates SQL data input file to pull in data from users, persons, companies, locations

--- db/sql/table_creation_dml/input_data_v2.sql
+++ db/sql/table_creation_dml/input_data_v2.sql
@@ -4,9 +4,34 @@ USE linkedin_group_5;
 
 
 #	LOAD DATA INTO THE USERS TABLE
-LOAD DATA LOCAL INFILE '../input_data/v2/user_input_v2_fixed.csv' INTO TABLE user
+LOAD DATA LOCAL INFILE '../input_data/v3/users.csv' INTO TABLE user
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (username, email);
+
+
+#	Load DATA INTO THE PERSONS TABLE
+LOAD DATA LOCAL INFILE '../input_data/v3/person.csv' INTO TABLE person
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(uid, firstname, middlename, lastname, birth_date, gender);
+
+#	LOAD LOCATION DATA
+LOAD DATA LOCAL INFILE '../input_data/v3/locations.csv' INTO TABLE location
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(coord_lat, coord_long, stree_address, state, zip, city);
+
+#	LOAD COMPANY DATA
+LOAD DATA LOCAL INFILE '../input_data/v3/company.csv' INTO TABLE company 
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(uid, lid, name, description);
