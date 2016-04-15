7ef8ab7fc36aced2fda89098cc0b6c7a38a5b4eb
Tony Forsythe <forsythetony@gmail.com>
2016-04-15 05:31:11 -0500

    Auth set

--- www/api/authorization.php
+++ www/api/authorization.php
@@ -47,9 +47,10 @@
 					
 					$stmt = $db_conn->stmt_init();
 					
-					$sql_query = "SELECT U.uid, U.username, UA.password_hash as hash, UA.salt as salt FROM user U, user_authentication UA WHERE U.uid = UA.uid AND U.username = '?' LIMIT 1";
+					echo "hi0";
+					$sql_query = "SELECT U.uid, U.username, UA.password_hash as hash, UA.salt as salt FROM user U, user_authentication UA WHERE U.uid = UA.uid AND U.username = '?'  LIMIT 1";
 					
-					
+					echo "hi1";
 					if ($stmt->prepare($sql_query)) {
 						
 						
