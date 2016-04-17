ee914def0f0674174b7e0eefc4012379a476862b
Tony Forsythe <forsythetony@gmail.com>
2016-04-17 15:38:32 -0500

    updates

--- db/sql/database_creation/input_data_v2.sql
+++ db/sql/database_creation/input_data_v2.sql
@@ -6,7 +6,7 @@ USE linkedin_group_5;
 #	LOAD DATA INTO THE USERS TABLE
 LOAD DATA LOCAL INFILE '../../input_data/v3/users.csv' INTO TABLE user
 FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
+OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (username, email);
@@ -15,7 +15,7 @@ IGNORE 1 LINES
 #	Load DATA INTO THE PERSONS TABLE
 LOAD DATA LOCAL INFILE '../../input_data/v3/person.csv' INTO TABLE person
 FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
+OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (uid, firstname, middlename, lastname, birth_date, gender);
@@ -23,7 +23,7 @@ IGNORE 1 LINES
 #	LOAD LOCATION DATA
 LOAD DATA LOCAL INFILE '../../input_data/v3/locations.csv' INTO TABLE location
 FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
+OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (coord_lat, coord_long, street_address, country, state, zip, city);
@@ -31,7 +31,7 @@ IGNORE 1 LINES
 #	LOAD COMPANY DATA
 LOAD DATA LOCAL INFILE '../../input_data/v3/company.csv' INTO TABLE company 
 FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
+OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (uid, lid, name, description);
