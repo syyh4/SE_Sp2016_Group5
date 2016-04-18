97f4d248fad1ea3c4b7eca25252bc22236ca62ea
thustonw <thustonw@gmail.com>
2016-04-17 20:13:19 -0500

    Update register.php

--- www/register.php
+++ www/register.php
@@ -1,4 +1,15 @@
 <?php
+
+if (isset($_POST['username']) && isset($_POST['password'])){
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['confirmpassword'];
+
+if ($password != $cpassword){
+         $msg = "passwords doesn't match";
+    }
+}
 	?>
 	
 	
