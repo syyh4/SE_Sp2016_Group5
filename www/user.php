f022e21efb49c7492e7ec1695360ad0db69342eb
LukeAnderson <lsa3m4@mail.missouri.edu>
2016-04-24 16:20:26 -0500

    Update user.php
    changed navbar href links so that they direct to the right php pages except for the logout

--- www/user.php
+++ www/user.php
@@ -101,13 +101,13 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">LinkedIn</a>
+          <a class="navbar-brand" href="http://52.165.38.69/home.php">LinkedIn</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">My Profile</a></li>
-            <li><a href="#">Search</a></li>
+            <li><a href="http://52.165.38.69/home.php">Home</a></li>
+            <li><a href="http://52.165.38.69/user.php">My Profile</a></li>
+            <li><a href="http://52.165.38.69/search.php">Search</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#">Logout</a></li>
