ecffdb80071946c4d083c31037018f2cddde5a88
Tony Forsythe <forsythetony@gmail.com>
2016-04-27 17:09:44 -0500

    Adds load user profile images script

---
+++ db/utility/load_user_profile_images.php
@@ -0,0 +1,161 @@
+<?php
+	
+	//	Include reference to sensitive databse information
+	//		Note:	This file should NOT be included in the public GitHub repository, it should only exist on the server.
+	include("../source_code/db_security/security.php");
+	
+	
+	//	First connect to the database using values from the included file
+	$db_conn = new mysqli(constant("DB_HOST"), constant("DB_USERNAME"), constant("DB_PASSWORD"), constant("DB_DATABASE"));
+	
+	if ($db_conn->error_code) {
+		
+		//	This should be replace PHP that sets the HTTP status code to 500 and
+		//	sets the body to the JSON object that contains the error_code and
+		//	error_string as defined by the API
+		die("The connection to the database failed: " . $db_conn->connect_error);
+	}
+	
+	
+	
+	$all_rand_ints = array();
+	
+	
+	$min_value = 0;
+	$max_value = 100;
+	
+	
+	$total_values = 50;
+	
+	echo "\n Min Value:\t$min_value\n Max Value:\t$max_value\n Total Values:\t$total_values\n";
+
+	for($i = 0; $i < $total_values; $i++) {
+		
+		$rand_int = rand($min_value, $max_value);
+		
+		array_push($all_rand_ints, $rand_int);
+	}
+
+	echo "\n\nEchoing Array Values...\n";
+	
+	
+	foreach( $all_rand_ints as $rand_int ) {
+		
+		echo "\nRand Int:\t$rand_int";
+	}
+	
+	echo "\n";
+
+
+
+
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+	/*
+		CUSTOM FUNCTIONS
+	*/
+	function generate_255_char_random_string() {
+		
+		$length = 64;
+		
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	    $charactersLength = strlen($characters);
+	    $randomString = '';
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, $charactersLength - 1)];
+	    }
+	    return $randomString;
+	}
+	function set_error_response( $error_code , $error_message ) {
+		
+		
+		$http_response_code = 500;
+		
+		$response_array = array(
+			"error_code" => $error_code,
+			"error_message" => $error_message
+		);
+				echo json_encode($response_array);
+
+		http_response_code($error_code);
+		
+	}
+	
+	function print_result_values( $result ) {
+		
+		$num_fields = $result->field_count;
+		
+		while ($row = $result->fetch_row()) {
+			
+			echo "\n";
+			for ($i = 0; $i < $num_fields; $i++) {
+				
+				echo $row[$i] . "\t\t";
+			}	
+		}
+		
+		echo "\n";
+	}
+	
+	function print_result_headers( $result ) {
+		
+		echo "\n";
+		
+		$num_fields = $result->field_count;
+		
+		$fields = $result->fetch_fields();
+		
+		for ($i = 0; $i < $num_fields; $i++) {
+			echo $fields[$i]->name . "\t\t";
+		}	
+	}
+	function print_result_all( $result ) {
+		
+		print_result_headers( $result );
+		print_result_values( $result );
+	}
+	//
+	//	Random Utility Functions
+	//
+	function execute_placeholder_query( $db_conn ) {
+		
+		//	First prepare the SQL query
+		$query_string = "SELECT * FROM user";
+		
+		if ($result = $db_conn->query($query_string)) {
+		
+			
+			print_result_all( $result );
+			
+			
+		}
+		else {
+			echo "Couldn't prepare the statement";
+		}
+	}	
+	//
+	//	Error Handling
+	//
+	function handle_request_error() {
+		
+		
+		
+		
+	}
+	
+	
+	
+	//	PHP automatically closes the connection to the DB when the script is finished running
+	//	so you don't have to do this yourself.
+	
+?>
