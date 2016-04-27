176a94b3bf0b929f4611ea884e85a34c4034edea
Ethan Hannah <ethan@e.local>
2016-04-27 08:07:32 -0500

    added nav bar

--- www/company.php
+++ www/company.php
@@ -89,6 +89,30 @@
 	</head>
 	
 	<body ng-app="myApp" ng-controller="CompanyController">
+
+    <nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="http://52.165.38.69/index.php">LinkedIn</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="http://52.165.38.69/home.php">Home</a></li>
+            <li><a href="http://52.165.38.69/user.php">My Profile</a></li>
+            <li><a href="http://52.165.38.69/search.php">Search</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://52.165.38.69/login.php">Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 		
 		<!-- ABOUT COMPANY
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
